1d19696378d44b152d5f4f90b98172ffe3d79a4a
Yuya Nishihara <yuya@tcha.org>
2011-11-05 19:33:47 +0900

    plantuml: bump version to 0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ['Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-plantuml',
-    version='0.1',
+    version='0.2',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-plantuml',
     license='BSD',
