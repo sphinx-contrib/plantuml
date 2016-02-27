b9d77003720fbe0cc7a6068dfc0941eb4df12f39
Yuya Nishihara <yuya@tcha.org>
2016-02-27 23:19:49 +0900

    plantuml: bump version to 0.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ['Sphinx>=1.0']
 
 setup(
     name='sphinxcontrib-plantuml',
-    version='0.6',
+    version='0.7',
     url='https://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='https://pypi.python.org/pypi/sphinxcontrib-plantuml',
     license='BSD',
