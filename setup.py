1ac1b454510708df496c723d71d31de71b657381
Yuya Nishihara <yuya@tcha.org>
2020-11-15 18:16:29 +0900

    bump version to 0.19

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ['Sphinx>=1.6']
 
 setup(
     name='sphinxcontrib-plantuml',
-    version='0.18.1',
+    version='0.19',
     url='https://github.com/sphinx-contrib/plantuml/',
     download_url='https://pypi.python.org/pypi/sphinxcontrib-plantuml',
     license='BSD',
